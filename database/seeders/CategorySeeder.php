40015940822fc93c77d83822fd8553a3e7512f68
tzh19 <thinzarhlaing04@gmail.com>
2025-11-12 20:28:18 +0630

    [add] create category, seed and factory.

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create 10 main categories
+        Category::factory(10)->create();
+
+        Category::factory(20)->create([
+            'parent_id' => function () {
+                return Category::inRandomOrder()->first()->id;
+            },
+        ]);
+
+    }
+}
