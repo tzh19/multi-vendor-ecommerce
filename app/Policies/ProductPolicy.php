b6c51d8bca46cbac3d399030f3e28c7317f4651d
tzh19 <thinzarhlaing04@gmail.com>
2026-01-05 20:06:18 +0630

    [add] product policy.

---
+++ app/Policies/ProductPolicy.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\Response;
+use App\Models\Product;
+use App\Models\User;
+
+class ProductPolicy
+{
+    /**
+     * Admin can do everything
+     */
+    public function before(User $user)
+    {
+        if ($user->isAdmin()) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+    * Vendor can view only their products
+    */
+    public function view(User $user, Product $product): bool
+    {
+        return $user->isVendor() && $product->vendor_id == $user->vendor->id;
+    }
+
+    /**
+    * Vendor can create products
+    */
+    public function create(User $user): bool
+    {
+        return $user->isVendor();
+    }
+
+    /**
+     * Vendor can update only their products
+     */
+
+    public function update(User $user, Product $product): bool
+    {
+        return $user->isVendor() && $product->vendor_id === $user->vendor->id;
+    }
+
+    /**
+     * Vendor can delete only their products
+     */
+    public function delete(User $user, Product $product): bool
+    {
+        return $user->isVendor() && $product->vendor_id === $user->vendor->id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Product $product): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Product $product): bool
+    {
+        return false;
+    }
+}
