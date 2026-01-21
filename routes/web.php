8338fa5ef30a53243bd89a922d3c85fc05502355
tzh19 <thinzarhlaing04@gmail.com>
2026-01-21 19:18:52 +0630

    [update] add Home.

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,8 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 use App\Http\Controllers\Admin\UserController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 
 Route::middleware(['auth', 'verified'])->group(function () {
 
