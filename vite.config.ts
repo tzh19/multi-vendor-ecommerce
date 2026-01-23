8e040e8c6eb04f08439370267f50c7fc5e43da99
tzh19 <thinzarhlaing04@gmail.com>
2026-01-23 20:25:48 +0630

    [update] vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,27 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
-import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 
 export default defineConfig({
-    base: process.env.VITE_APP_URL + '/', // ensures assets use HTTPS
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
             refresh: true,
+            server: {
+                origin: process.env.APP_URL,
+            },
+            build: {
+                manifest: true,
+            },
         }),
-        tailwindcss(),
-        wayfinder({ formVariants: true }),
         vue({
             template: {
                 transformAssetUrls: {
-                    base: null,
+                    base: process.env.APP_URL,
                     includeAbsolute: false,
                 },
             },
         }),
+        tailwindcss(),
     ],
 });
