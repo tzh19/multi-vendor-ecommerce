902fabda49da11bd63e0fe9307505ad7eee29afe
tzh19 <thinzarhlaing04@gmail.com>
2026-01-28 20:02:35 +0630

    [update] fix routes.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -40,9 +40,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+    @routes
     @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
     @inertiaHead
-    @routes
 </head>
 
 <body class="font-sans antialiased">
